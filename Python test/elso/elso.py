188c7234a729915a97969e74afcd13096a676b44
Zete00 <147609886+Zete00@users.noreply.github.com>
2024-10-03 18:00:56 +0200

    Add files via upload

---
+++ Python test/elso/elso.py
@@ -0,0 +1,9 @@
+Veznev = input("Add meg a vezeték neved: ")
+
+if Veznev == (""): print("Adj meg legalább 1 karaktert")
+
+Kernev = input("Add meg a kereszt neved: ")
+
+if Kernev == (""): print("Adj meg legalább 1 karaktert")
+
+else: print(f"Üdvözöllek {Veznev} {Kernev}!")
---
+++ Python test/masodik/MATHTEST.py
@@ -0,0 +1,8 @@
+import math
+
+x = int(input("Adj meg egy egész számot: "))
+
+szam1 = math.pow(x,3)
+szam2 = math.sqrt(x)
+
+print(szam1, szam2)
