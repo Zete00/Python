dca9b8f61c563a5d2f6f99ec79556f447c1424a5
Zete00 <147609886+Zete00@users.noreply.github.com>
2024-10-04 15:33:00 +0200

    Add files via upload

---
+++ Python test/Harmadik/RockPaperS.py
@@ -0,0 +1,26 @@
+import random
+
+RPS = ["Rock", "Paper", "Scissors"]
+valasz = input("Choose Rock, Paper or Scissors: ").capitalize().strip()
+x = random.choice(RPS)
+
+if (valasz != "Rock" or "Paper" or "Scissors"):
+    print("Give a chooseable answer.")
+elif (valasz == "Rock" and x == "Rock"):
+    print("Tie!")
+elif (valasz == "Rock" and x == "Paper"):
+    print("You lost!")
+elif (valasz == "Rock" and x == "Scissors"):
+    print("You win!")
+elif (valasz == "Paper" and x == "Rock"):
+    print("You win!")
+elif (valasz == "Paper" and x == "Paper"):
+    print("Tie")
+elif (valasz == "Paper" and x == "Scissors"):
+    print("You lost!")
+elif (valasz == "Scissors" and x == "Rock"):
+    print("You lost!")
+elif (valasz == "Scissors" and x == "Paper"):
+    print("You win!")
+else:
+    print("Tie!")
